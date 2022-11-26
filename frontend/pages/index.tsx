1b5fe02096c9b24573a9a7eb9bc21d2425d40851
Abbas Khan <78753275+Abbas-Khann@users.noreply.github.com>
2022-11-26 22:03:59 +0800

    Added Hero section

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,10 +1,12 @@
 import React from "react";
+import Hero from "../Components/Hero";
 import Navbar from "../Components/Navbar";
 
 const Home = (): JSX.Element => {
   return (
     <div className=" w-screen h-screen no-repeat bg-cover bg-[url('/img/landing-image.png')]">
-        <Navbar />  
+        <Navbar />
+        <Hero />
     </div>
   )
 }
