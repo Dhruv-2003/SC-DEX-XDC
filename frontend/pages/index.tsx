bbcbdcfd8dc338f9ca8b4c09ef93bffba0775588
Abbas Khan <78753275+Abbas-Khann@users.noreply.github.com>
2022-11-27 15:56:07 +0800

    changed color on background on mobile

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -6,7 +6,9 @@ import Navbar from "../Components/Navbar";
 const Home = (): JSX.Element => {
   const [expand, setExpand] = useState<boolean>(false);
   return (
-    <div className="w-screen min-h-screen no-repeat bg-cover bg-[url('/img/landing-image.png')]">
+    <div 
+    className=
+    {`w-screen min-h-screen no-repeat bg-cover ${!expand ? `bg-[url('/img/landing-image.png')]` : `bg-black`}`}>
         <Navbar expand={expand} setExpand={setExpand}/>
         { expand ? null : <Hero /> }
         { expand ? null : <Footer /> }
