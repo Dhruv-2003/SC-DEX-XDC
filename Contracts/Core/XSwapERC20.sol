bc2252bd6225237970fc2a2d7790be9dc62e731b
Dhruv agarwal <dhruv@Dhruvs-MacBook-Pro.local>
2022-12-12 19:58:32 +0530

    XSwapToken created

--- Contracts/Core/XSwapERC20.sol
+++ Contracts/Core/XSwapERC20.sol
@@ -6,17 +6,13 @@ import "@openzeppelin/contracts/utils/maths/SafeMath.sol";
 contract XSwapERC20 {
     using SafeMath for uint256;
 
-    string public constant name = "Fswap V1";
-    string public constant symbol = "F-V1";
+    string public constant name = "XSwap-V1";
+    string public constant symbol = "XS-V1";
     uint8 public constant decimals = 18;
     uint256 public totalSupply;
     mapping(address => uint256) public balanceOf;
     mapping(address => mapping(address => uint256)) public allowance;
 
-    bytes32 public DOMAIN_SEPARATOR;
-    // keccak256("Permit(address owner,address spender,uint256 value,uint256 nonce,uint256 deadline)");
-    bytes32 public constant PERMIT_TYPEHASH =
-        0x6e71edae12b1b97f4d1f60370fef10105fa2faae0126114a169c64845d6126c9;
     mapping(address => uint256) public nonces;
 
     event Approval(
@@ -26,23 +22,7 @@ contract XSwapERC20 {
     );
     event Transfer(address indexed from, address indexed to, uint256 value);
 
-    constructor() public {
-        uint256 chainId;
-        assembly {
-            chainId := chainid
-        }
-        DOMAIN_SEPARATOR = keccak256(
-            abi.encode(
-                keccak256(
-                    "EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)"
-                ),
-                keccak256(bytes(name)),
-                keccak256(bytes("1")),
-                chainId,
-                address(this)
-            )
-        );
-    }
+    constructor() public {}
 
     function _mint(address to, uint256 value) internal {
         totalSupply = totalSupply.add(value);
@@ -97,39 +77,5 @@ contract XSwapERC20 {
         }
         _transfer(from, to, value);
         return true;
-    }
-
-    function permit(
-        address owner,
-        address spender,
-        uint256 value,
-        uint256 deadline,
-        uint8 v,
-        bytes32 r,
-        bytes32 s
-    ) external {
-        require(deadline >= block.timestamp, "UniswapV2: EXPIRED");
-        bytes32 digest = keccak256(
-            abi.encodePacked(
-                "\x19\x01",
-                DOMAIN_SEPARATOR,
-                keccak256(
-                    abi.encode(
-                        PERMIT_TYPEHASH,
-                        owner,
-                        spender,
-                        value,
-                        nonces[owner]++,
-                        deadline
-                    )
-                )
-            )
-        );
-        address recoveredAddress = ecrecover(digest, v, r, s);
-        require(
-            recoveredAddress != address(0) && recoveredAddress == owner,
-            "UniswapV2: INVALID_SIGNATURE"
-        );
-        _approve(owner, spender, value);
     }
 }
