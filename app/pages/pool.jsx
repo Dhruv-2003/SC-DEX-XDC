e452bf1c03a3b7505079575f7d04154ed373f551
kushagrasarathe <76868364+kushagrasarathe@users.noreply.github.com>
2022-12-17 14:44:41 +0530

    pool page

--- app/pages/pool.jsx
+++ app/pages/pool.jsx
@@ -2,14 +2,14 @@ import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
 import { Fragment, useState } from "react";
 import { CheckIcon, ChevronUpDownIcon } from "@heroicons/react/20/solid";
-import { tokens } from "../utils/tokens";
 import { Dialog, Listbox, Transition } from "@headlessui/react";
-import styles from '../styles/Home.module.css'
+import styles from "../styles/Home.module.css";
+import { tokens } from "../utils/tokens";
 
 const token1 = tokens;
 const token2 = tokens;
 
-export default function Swap() {
+export default function Pool() {
   const [expand, setExpand] = useState(false);
   const [selectedToken1, setSelectedToken1] = useState(token1[0]);
   const [selectedToken2, setSelectedToken2] = useState(token2[0]);
@@ -17,39 +17,32 @@ export default function Swap() {
   return (
     <div
       className={`w-screen min-h-screen no-repeat bg-cover bg-[#03071E]
-        ${!expand ? `${styles.bg} bg-[url('../assets/landing.png')]` : `bg-[#03071E]`}
+        ${
+          !expand
+            ? `${styles.bg2} bg-[url('../assets/landing.png')]`
+            : `bg-[#03071E]`
+        }
           `}
     >
       <Navbar expand={expand} setExpand={setExpand} />
       {expand ? null : (
-        <div className=" w-full flex justify-center items-center px-2">
-          <div className=" max-w-xl rounded-md mx-auto lg:mx-auto font-fredoka text-white px-0 py-0 bg-[#03071e68] opacity-100 backdrop-blur-lg flex flex-col items-center justify-center mt-32 md:mt-12 xl:mt-32 2xl:mt-40 mb-32 ">
-            <h2 className=" rounded-t-md text-xl font-semibold tracking-wid w-full bg-[blue-700] py-4 px-4 border-b border-gray-400">
-              Swap Tokens
-            </h2>
-            <div className=" px-4 py-8">
-              <label className="" htmlFor="">
-                Enter Value
-              </label>
-              <div className="mt-2 w-full flex items-center justify-between mb-2">
-                <input
-                  type="number"
-                  id=""
-                  className="bg-gray-50 border  lg:w-full w-44 border-gray-300 text-gray-900 text-sm rounded-md focus:ring-blue-500 focus:border-blue-500 block  p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                  placeholder="0"
-                  required
-                />
-                <div className="lg:w-28 w-24 "></div>
-                {/* <button
-                  //   onClick={openModal}
-                  type="button"
-                  className="text-white mt-1  bg-blue-700 text-sm hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-md lg:w-48 px-4 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
-                >
-                  Select Token
-                </button> */}
+        <div className=" w-full mt-10 flex flex-col justify-center items-center px-2">
+          <div className="w-full flex justify-around">
+            <h1 className=" text-gray-100 text-3xl font-semibold">Pools</h1>
+            <button className="active:scale-105 bg-[#fc6f38] px-3 py-2 text-sm font-semibold rounded-md">
+              + New Pool
+            </button>
+          </div>
+          <div className="mt-8 lg:w-7/12 border rounded-lg border-gray-500 px-4 py-6 bg-transparent backdrop-blur-xl">
+            <span className=" text-gray-100 text-lg font-semibold">
+              Select Pair
+            </span>
+            <div className="lg:w-full flex items-center justify-between ">
+              {/* token1 */}
+              <div className="">
                 <Listbox value={selectedToken1} onChange={setSelectedToken1}>
                   <div className="relative mt-1">
-                    <Listbox.Button className="relative  cursor-default rounded-md w-36 lg:w-36 px-4 py-2.5 bg-gray-700 pl-3 pr-10 text-left shadow-md focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm">
+                    <Listbox.Button className="relative  cursor-default rounded-md w-36 lg:w-36 px-4 py-2.5 bg-gray-800 text-white pl-3 pr-10 text-left shadow-md focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm">
                       <span className="block truncate">
                         {selectedToken1.symbol}
                       </span>
@@ -108,29 +101,11 @@ export default function Swap() {
                 </Listbox>
               </div>
 
-              <label className="mt-6" htmlFor="">
-                Enter Value
-              </label>
-
-              <div className=" w-full flex items-center justify-between ">
-                <input
-                  type="number"
-                  id=""
-                  className="mt-2  bg-gray-50 border lg:w-full w-44 border-gray-300 text-gray-900 text-sm rounded-md focus:ring-blue-500 focus:border-blue-500 block  p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                  placeholder="0"
-                  required
-                />
-                <div className="lg:w-28 w-24 "></div>
-                {/* <button
-                  //   onClick={openModal}
-                  type="button"
-                  className="text-white mt-1  bg-blue-700 text-sm hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-md lg:w-48 px-4 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
-                >
-                  Select Token
-                </button> */}
+              {/* token2 */}
+              <div className="ml-4">
                 <Listbox value={selectedToken2} onChange={setSelectedToken2}>
                   <div className="relative mt-1">
-                    <Listbox.Button className="relative cursor-default rounded-md w-36 lg:w-36 px-4 py-2.5 bg-gray-700 pl-3 pr-10 text-left shadow-md focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm">
+                    <Listbox.Button className="relative cursor-default rounded-md w-36 lg:w-36 px-4 py-2.5 bg-gray-800 text-white pl-3 pr-10 text-left shadow-md focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm">
                       <span className="block truncate">
                         {selectedToken2.symbol}
                       </span>
@@ -188,16 +163,47 @@ export default function Swap() {
                   </div>
                 </Listbox>
               </div>
-
-              <div className="px-2 border-t border-gray-400 pt-6 w-full mt-6 mx-auto">
-              <button
-              type="button"
-              className="text-white w-full bg-orange-600 text-md font-fredoka active:bg-orange-700 font-medium rounded-sm px-5 py-2.5 mr-2 mb-2"
-            >
-              Swap
-            </button>
-              </div>
             </div>
+            <div class="mt-4 relative pt-1 flex flex-col">
+              <label for="customRange3" className=" text-white">
+                0.05 % fee tier
+              </label>
+              <input
+                type="range"
+                className="mt-3 lg:w-full h-2 bg-orange-500 rounded-lg appearance-none cursor-pointer form-range p-0 bg-transparent focus:outline-none focus:ring-0 focus:shadow-none"
+                min="0"
+                max="3"
+                step="1"
+                id="customRange3"
+              />
+            </div>
+            <div class="mt-4 relative pt-1 flex flex-col">
+              <span className=" text-gray-100 text-lg font-semibold">
+                Deposit Amounts
+              </span>
+
+              <input
+                type="number"
+                id=""
+                className="mt-3 bg-gray-800 text-white border  lg:w-full border-gray-300  text-sm rounded-md focus:ring-blue-500 focus:border-blue-500 block  p-2.5 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder="token 1"
+                required
+              />
+
+              <input
+                type="number"
+                id=""
+                className="mt-3 bg-gray-800 text-white border  lg:w-full border-gray-300  text-sm rounded-md focus:ring-blue-500 focus:border-blue-500 block  p-2.5 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder="token 2"
+                required
+              />
+            </div>
+            <button
+              type="button"
+              className="text-white w-full mt-4 bg-orange-600 text-md font-fredoka active:bg-orange-700 font-medium rounded-sm px-5 py-2.5 mr-2 mb-2"
+            >
+              Create Pool
+            </button>
           </div>
         </div>
       )}
