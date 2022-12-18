6590d8b6176559307c9636261d18a31d8fc1d290
kushagrasarathe <76868364+kushagrasarathe@users.noreply.github.com>
2022-12-18 12:03:11 +0530

    stake page

--- app/pages/stake.jsx
+++ app/pages/stake.jsx
@@ -1,7 +1,219 @@
-import React from 'react'
+import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
+import { Fragment, useState } from "react";
+import { CheckIcon, ChevronUpDownIcon } from "@heroicons/react/20/solid";
+import { Dialog, Listbox, Transition } from "@headlessui/react";
+import styles from "../styles/Home.module.css";
+import { tokens } from "../utils/tokens";
 
-export default function stake() {
+const token1 = tokens;
+const token2 = tokens;
+
+export default function Stake() {
+  const [expand, setExpand] = useState(false);
+  const [toggle, setToggle] = useState(false);
+  const [selectedToken1, setSelectedToken1] = useState(token1[0]);
+  const [selectedToken2, setSelectedToken2] = useState(token2[0]);
+
+  const newPool = () => {
+    setToggle(!toggle)
+  }
+
+
   return (
-    <div>stake</div>
-  )
+    <div
+      className={`w-screen min-h-screen no-repeat bg-cover bg-[#03071E]
+        ${
+          !expand
+            ? `${styles.bg2} bg-[url('../assets/landing.png')]`
+            : `bg-[#03071E]`
+        }
+          `}
+    >
+      <Navbar expand={expand} setExpand={setExpand} />
+      {expand ? null : (
+        <div className=" w-full mt-10 flex flex-col justify-center items-center px-2">
+          <div className="w-full flex justify-around">
+            <h1 className=" text-gray-100 text-3xl font-semibold">Pools</h1>
+            <button onClick={newPool} className="active:scale-95 bg-[#fc6f38] px-3 py-2 text-sm font-semibold rounded-md">
+              + New Pool
+            </button>
+          </div>
+          <div className={`${toggle ? `visible` : `hidden` } mt-8 lg:w-7/12 border rounded-lg border-gray-500 px-4 py-6 bg-transparent backdrop-blur-xl`}>
+            <span className=" text-gray-100 text-lg font-semibold">
+              Select Pair
+            </span>
+            <div className="lg:w-5/12 flex items-center justify-start ">
+              {/* token1 */}
+              <div className="">
+                <Listbox value={selectedToken1} onChange={setSelectedToken1}>
+                  <div className="relative mt-1">
+                    <Listbox.Button className="relative  cursor-default rounded-md w-36 lg:w-36 px-4 py-2.5 bg-gray-800 text-white pl-3 pr-10 text-left shadow-md focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm">
+                      <span className="block truncate">
+                        {selectedToken1.symbol}
+                      </span>
+                      <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
+                        <ChevronUpDownIcon
+                          className="h-5 w-5 text-gray-200"
+                          aria-hidden="true"
+                        />
+                      </span>
+                    </Listbox.Button>
+                    <Transition
+                      as={Fragment}
+                      leave="transition ease-in duration-100 "
+                      leaveFrom="opacity-100"
+                      leaveTo="opacity-0"
+                    >
+                      <Listbox.Options className="absolute mt-1 max-h-60 w-full z-[100] overflow-auto rounded-md  bg-transparent backdrop-blur-xl py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+                        {tokens.map((token, tokenId) => (
+                          <Listbox.Option
+                            key={tokenId}
+                            className={({ active }) =>
+                              `relative cursor-default select-none py-2 pl-10 pr-4 ${
+                                active
+                                  ? "bg-gray-100 text-gray-900"
+                                  : "text-gray-100"
+                              }`
+                            }
+                            value={token}
+                          >
+                            {({ selectedToken1 }) => (
+                              <>
+                                <span
+                                  className={`block truncate ${
+                                    selectedToken1
+                                      ? "font-medium"
+                                      : "font-normal"
+                                  }`}
+                                >
+                                  {token.symbol}
+                                </span>
+                                {selectedToken1 ? (
+                                  <span className="absolute inset-y-0 left-0 flex items-center pl-3 text-amber-600">
+                                    <CheckIcon
+                                      className="h-5 w-5 text-gray-900"
+                                      aria-hidden="true"
+                                    />
+                                  </span>
+                                ) : null}
+                              </>
+                            )}
+                          </Listbox.Option>
+                        ))}
+                      </Listbox.Options>
+                    </Transition>
+                  </div>
+                </Listbox>
+              </div>
+
+              {/* token2 */}
+              <div className="ml-4 lg:ml-7">
+                <Listbox value={selectedToken2} onChange={setSelectedToken2}>
+                  <div className="relative mt-1">
+                    <Listbox.Button className="relative cursor-default rounded-md w-36 lg:w-36 px-4 py-2.5 bg-gray-800 text-white pl-3 pr-10 text-left shadow-md focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm">
+                      <span className="block truncate">
+                        {selectedToken2.symbol}
+                      </span>
+                      <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
+                        <ChevronUpDownIcon
+                          className="h-5 w-5 text-gray-200"
+                          aria-hidden="true"
+                        />
+                      </span>
+                    </Listbox.Button>
+                    <Transition
+                      as={Fragment}
+                      leave="transition ease-in duration-100"
+                      leaveFrom="opacity-100"
+                      leaveTo="opacity-0"
+                    >
+                      <Listbox.Options className="absolute mt-1 max-h-60 w-full overflow-auto rounded-md  bg-transparent backdrop-blur-xl py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+                        {tokens.map((token, tokenId) => (
+                          <Listbox.Option
+                            key={tokenId}
+                            className={({ active }) =>
+                              `relative cursor-default select-none py-2 pl-10 pr-4 ${
+                                active
+                                  ? "bg-gray-100 text-gray-900"
+                                  : "text-gray-100"
+                              }`
+                            }
+                            value={token}
+                          >
+                            {({ selectedToken2 }) => (
+                              <>
+                                <span
+                                  className={`block truncate ${
+                                    selectedToken2
+                                      ? "font-medium"
+                                      : "font-normal"
+                                  }`}
+                                >
+                                  {token.symbol}
+                                </span>
+                                {selectedToken2 ? (
+                                  <span className="absolute inset-y-0 left-0 flex items-center pl-3 text-amber-600">
+                                    <CheckIcon
+                                      className="h-5 w-5 text-gray-900"
+                                      aria-hidden="true"
+                                    />
+                                  </span>
+                                ) : null}
+                              </>
+                            )}
+                          </Listbox.Option>
+                        ))}
+                      </Listbox.Options>
+                    </Transition>
+                  </div>
+                </Listbox>
+              </div>
+            </div>
+            <div class="mt-4 relative pt-1 flex flex-col">
+              <label for="customRange3" className=" text-white">
+                0.05 % fee tier
+              </label>
+              <input
+                type="range"
+                className="mt-3 lg:w-full h-2 bg-orange-500 rounded-lg appearance-none cursor-pointer form-range p-0 bg-transparent focus:outline-none focus:ring-0 focus:shadow-none"
+                min="0"
+                max="3"
+                step="1"
+                id="customRange3"
+              />
+            </div>
+            <div class="mt-4 relative pt-1 flex flex-col">
+              <span className=" text-gray-100 text-lg font-semibold">
+                Deposit Amounts
+              </span>
+
+              <input
+                type="number"
+                id=""
+                className="mt-3 bg-gray-800 text-white border  lg:w-full border-gray-300  text-sm rounded-md focus:ring-blue-500 focus:border-blue-500 block  p-2.5 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder="token 1"
+                required
+              />
+
+              <input
+                type="number"
+                id=""
+                className="mt-3 bg-gray-800 text-white border  lg:w-full border-gray-300  text-sm rounded-md focus:ring-blue-500 focus:border-blue-500 block  p-2.5 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder="token 2"
+                required
+              />
+            </div>
+            <button
+              type="button"
+              className="text-white w-full mt-6 bg-orange-600 text-md font-fredoka active:bg-orange-700 font-medium rounded-sm px-5 py-2.5 mr-2 mb-2"
+            >
+              Create Pool
+            </button>
+          </div>
+        </div>
+      )}
+      <Footer />
+    </div>
+  );
 }
