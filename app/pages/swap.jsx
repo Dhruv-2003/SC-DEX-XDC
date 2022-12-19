a9ec721af388459f4e7cf02c7629db6e4fde5d0c
kushagrasarathe <76868364+kushagrasarathe@users.noreply.github.com>
2022-12-19 22:29:10 +0530

    rename

--- app/pages/swap.jsx
+++ app/pages/swap.jsx
@@ -11,7 +11,7 @@ import {
   SWAP_ROUTER_ADDRESS,
   SWAP_ROUTER_ABI,
   Token_ABI,
-} from "../Constants/index.js";
+} from "../constants/index.js";
 
 const token1 = tokens;
 const token2 = tokens;
